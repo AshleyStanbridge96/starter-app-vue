bf2400a38ccc25dd11f16e106f6a6d3537d8c906
Ashley Stanbridge <ashley_stanbridge@yahoo.com>
2019-10-11 12:25:29 -0500

    Changed inner html to reflect vue result. Deleted adder folder and replaced with sectionEstimator.js file within components folder.

---
+++ components/sectionEstimator.js
@@ -0,0 +1,27 @@
+const calcAvg = (numStudent, numSection) => {
+    const avg = (numStudent / numSection);
+    return avg;
+}
+
+
+//Creating new vue called findTip
+const findAvg = new Vue ({
+    el: '#findAvg',
+    //Putting default values. Use v-model name from HTML page (name ID same as v-model)
+    data:{
+        numStudent: 30,
+        numSection: 4,
+    },
+    /*Computed is where the meat of the vue is. Here, you will do the normal parseInt stuff here but use this.bill_amt
+    instead of using parseFloat(document.querySelector("#bill-amt").value). Then, use that logic to pass into
+    functions made at the top and store that in an overall result to display on web page*/
+    computed: {
+        avgTotal: function(){
+            const numStudent = parseInt(this.numStudent)
+            const numSection = parseFloat(this.numSection)
+
+            const avg = calcAvg(numStudent, numSection)
+            return `Average is: $ ${avg}`
+        }
+    }
+})
